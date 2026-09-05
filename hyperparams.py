d6c58b96ed8b26f3150067204c32cd1829372454
Mayuk Jain <f20250149@pilani.bits-pilani.ac.in>
2026-09-05 17:54:27 +0530

    Update hyperparams

--- hyperparams.py
+++ hyperparams.py
@@ -13,23 +13,3 @@ window_size=128
 mask_type = 'bigbird' 
 num_global=2
 num_random=3
-
-
-## testing ke liya to chnage
-'''
-batch_size = 16
-block_size = 32
-max_iter = 200
-eval_interval = 100
-learn_rate = 3e-4
-device = "cuda"
-eval_itr = 20
-n_embd = 64
-n_layer = 2
-n_head = 2
-dropout = 0.0
-
-window_size = 8
-mask_type = 'bigbird'
-num_global = 2
-num_random = 3'''
